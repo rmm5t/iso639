f66db5e07194fc17045addaa4a1664613acf2a81
Ryan McGeary <ryan@mcgeary.org>
2012-12-24 12:50:49 -0500

    Switched from minitest-emoji to minitest-pride

--- iso639.gemspec
+++ iso639.gemspec
@@ -18,6 +18,5 @@ Gem::Specification.new do |gem|
   gem.require_paths = ["lib"]
 
   gem.add_development_dependency "minitest",       "~> 4.3"
-  gem.add_development_dependency "minitest-emoji", "~> 1.0"
   gem.add_development_dependency "rake",           "~> 10.0"
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,3 @@
 require "minitest/autorun"
-require "minitest/emoji"
+require "minitest/pride"
 require "iso639"
-
-# Borrowed from https://gist.github.com/4369168
-MiniTest::Emoji::DEFAULT.merge!(
-  '.' => "\u{1f49A} ",
-  'F' => "\u{1f494} ",
-  'E' => "\u{1f480} ",
-  'S' => "\u{1f49B} "
-)
