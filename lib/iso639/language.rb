34d60380729a3633ccb0a7f718d1a851bb03d870
Ryan McGeary <ryan@mcgeary.org>
2012-12-28 14:30:48 -0500

    Refactored language class and updated rdoc

--- lib/iso639/language.rb
+++ lib/iso639/language.rb
@@ -8,30 +8,31 @@ module Iso639
     # alpha3_bibliographic - A String representing the alpha-3 bibliographic code
     # alpha3_terminology - A String representing the alpha-3 terminology code
     # alpha2 - A String representing the alpha-2 code
-    # english_name - A String representing the English name
-    # french_name - A String representing the French name
-    def initialize(alpha3_bibliographic, alpha3_terminology, alpha2, english_name, french_name)
+    # english_names - A String representing the English names (semi-colon delimited)
+    # french_names - A String representing the French names (semi-colon delimited)
+    def initialize(alpha3_bibliographic, alpha3_terminology, alpha2, english_names, french_names)
       @alpha3_bibliographic = strip_to_nil(alpha3_bibliographic)
       @alpha3_terminology   = strip_to_nil(alpha3_terminology)
       @alpha2               = strip_to_nil(alpha2)
-      @english_names        = english_name.split(/;\s*/).map { |n| strip_to_nil(n) }
-      @french_names         = french_name.split(/;\s*/).map { |n| strip_to_nil(n) }
+      @english_names        = split_and_strip(english_names)
+      @french_names         = split_and_strip(french_names)
     end
 
     def english_name
       english_names.first
     end
     alias_method :name, :english_name
+    alias_method :to_s, :english_name
 
     def french_name
       french_names.first
     end
 
-    def to_s
-      name
-    end
-
     private
+
+    def split_and_strip(names)
+      names.split(/;\s*/).map { |n| strip_to_nil(n) }
+    end
 
     def strip_to_nil(val)
       val = val.strip if val
